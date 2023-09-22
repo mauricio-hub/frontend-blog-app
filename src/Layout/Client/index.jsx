4f46a72d46871f52e02a0557bb949c03eaa1a2ff
mauricio-hub <mauriciogonzalezdeveloper@gmail.com>
2023-09-21 23:02:11 -0500

    agregando content al layout

--- src/Layout/Client/index.jsx
+++ src/Layout/Client/index.jsx
@@ -1,5 +1,4 @@
-// Layout.js
-import React, { useEffect, useState } from "react";
+/* import React, { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import { Footer } from "../../components/Footer";
 
@@ -7,8 +6,7 @@ export const Layout = ({ children }) => {
     const [theme, setTheme] = useState("light");
 
     useEffect(() => {
-      // No es necesario manipular el DOM directamente aquí
-      // Solo cambia el estado del tema
+
       if (theme === "dark") {
         document.documentElement.classList.add("dark");
       } else {
@@ -20,10 +18,79 @@ export const Layout = ({ children }) => {
       setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
     };
   return (
-    <div className={`h-screen App ${theme === "dark" ? "dark-bg" : "light-bg"}`}>
+    <div className={`h-screen App ${theme === "dark" ? "dark-bg" : "light-bg"}`} >
         <Header theme={theme} onChangeTheme={handleChangeTheme} />
       <main>{children}</main>
       <Footer />
+    </div>
+  );
+}; */
+
+/* import React, { useEffect, useState } from "react";
+import Header from "../../components/Header";
+import { Footer } from "../../components/Footer";
+
+export const Layout = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    // No es necesario manipular el DOM directamente aquí
+    // Solo cambia el estado del tema
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleChangeTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <div className="max-w-screen-xl mx-auto px-4 sm:px-6 md:px-8">
+    <div className={`h-screen ${theme === "dark" ? "dark-bg" : "light-bg"}`}>
+      <Header theme={theme} onChangeTheme={handleChangeTheme} />
+    
+      
+        <main>{children}</main>
+      
+      <Footer />
+      </div>
+    </div>
+  );
+};
+ */
+
+import React, { useEffect, useState } from "react";
+import Header from "../../components/Header";
+import { Footer } from "../../components/Footer";
+
+export const Layout = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    // No es necesario manipular el DOM directamente aquí
+    // Solo cambia el estado del tema
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleChangeTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <div className={`min-h-screen ${theme === "dark" ? "dark-bg" : "light-bg"}`}>
+      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 md:px-8">
+      <Header theme={theme} onChangeTheme={handleChangeTheme} />
+        <main>{children}</main>
+     
+      <Footer />
+      </div>
     </div>
   );
 };
