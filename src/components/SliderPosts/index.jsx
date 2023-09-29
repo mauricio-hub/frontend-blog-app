89fec7b16a0acc655705ce2cada80662b9efe3cb
mauricio-hub <mauriciogonzalezdeveloper@gmail.com>
2023-09-29 12:58:37 -0500

    agregando paginacion

--- src/components/SliderPosts/index.jsx
+++ src/components/SliderPosts/index.jsx
@@ -1,40 +1,183 @@
-import React from 'react';
+import React, { useState } from "react";
 
 export const SliderPost = () => {
-    // Datos de ejemplo para las publicaciones
-    const posts = [
-      { id: 1, title: 'Publicación 1', image: 'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=801' },
-      { id: 2, title: 'Publicación 2', image: 'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' },
-      { id: 3, title: 'Publicación 3', image: 'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' },
-      { id: 4, title: 'Publicación 4', image: 'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' },
-      { id: 5, title: 'Publicación 5', image: 'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' },
-      { id: 6, title: 'Publicación 6', image: 'https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80' },
-      
-   
-    ];
-  
-    return (
-      <>
-          <h2 >All blog post</h2>
-      <div className="py-12 md:px-16 sm:px-14 px-6">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {posts.map((post) => (
-            <div key={post.id} className="p-2">
-              <div className="">
-                <img
-                  src={post.image}
-                  alt={post.title}
-                  className="mx-auto object-cover "
-                />
-                <p>20-11-2023</p>
-                <h2 className="text-lg mt-2">{post.title}</h2>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis iure facilis, odit adipisci vel molestiae? Possimus molestias excepturi veritatis voluptatibus!</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-      </>
-    );
+  // Datos de ejemplo para las publicaciones
+  const posts = [
+    {
+      id: 1,
+      title: " 1",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=801",
+    },
+    {
+      id: 2,
+      title: " 2",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 3,
+      title: " 3",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 4,
+      title: " 4",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 5,
+      title: "5",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 6,
+      title: "Publicación 6",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 7,
+      title: " 7",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 8,
+      title: "8",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 9,
+      title: "9",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 10,
+      title: "10",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+
+
+    {
+      id: 11,
+      title: "11",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 12,
+      title: "12",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 13,
+      title: "13",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 14,
+      title: "14",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 15,
+      title: "15",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 16,
+      title: " 16",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 17,
+      title: " 17",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 18,
+      title: "18",
+      image:
+        "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    },
+  ];
+
+
+  const itemsPerPage = 6; // Número de elementos por página
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Calcular el índice de inicio y final de la página actual
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  // Obtener las publicaciones de la página actual
+  const currentPosts = posts.slice(startIndex, endIndex);
+
+  // Función para cambiar de página
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
   };
 
+  return (
+     <div style={{ marginTop: "80px" }}>
+      <h2>All blog posts</h2>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6" style={{ marginTop: "20px" }}>
+        {currentPosts.map((post) => (
+          <div>
+          <img
+            src={post.image}
+            alt={post.title}
+            className="mx-auto object-cover"
+          />
+          <p>20-11-2023</p>
+          <h2 className="text-lg mt-2">{post.title}</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Corporis iure facilis, odit adipisci vel molestiae? Possimus
+            molestias excepturi veritatis voluptatibus!
+          </p>
+          <span class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">
+            Red
+          </span>
+          <span class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+            Green
+          </span>
+        </div>
+        ))}
+      </div>
+
+      {/* Paginación */}
+      <div className="flex justify-center mt-4">
+        {[...Array(Math.ceil(posts.length / itemsPerPage)).keys()].map((page) => (
+          <button
+            key={page}
+            style={{
+              background: page + 1 === currentPage ? "#F9F5FF" : "#dadada",
+              color: page + 1 === currentPage ? "#6a36be" : "#000000",
+              padding: "0.5rem 1rem",
+              margin: "0.25rem",
+              borderRadius: "0.25rem",
+            }}
+            onClick={() => handlePageChange(page + 1)}
+          >
+            {page + 1}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
